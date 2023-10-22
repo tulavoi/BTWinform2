acae2c61d05d2492f7c2e97c19e38c8bdc0cffd2
tulavoi <148041452+tulavoi@users.noreply.github.com>
2023-10-22 19:04:08 +0700

    Add files via upload

---
+++ Bai8/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Bai8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tvShow = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFN = new System.Windows.Forms.TextBox();
+            this.txtLN = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.img = new System.Windows.Forms.ImageList(this.components);
+            this.btnDel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tvShow
+            // 
+            this.tvShow.ImageIndex = 0;
+            this.tvShow.ImageList = this.img;
+            this.tvShow.Location = new System.Drawing.Point(12, 12);
+            this.tvShow.Name = "tvShow";
+            this.tvShow.SelectedImageIndex = 0;
+            this.tvShow.Size = new System.Drawing.Size(316, 345);
+            this.tvShow.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDel);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtLN);
+            this.groupBox1.Controls.Add(this.txtFN);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(348, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 261);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // txtFN
+            // 
+            this.txtFN.Location = new System.Drawing.Point(6, 55);
+            this.txtFN.Name = "txtFN";
+            this.txtFN.Size = new System.Drawing.Size(239, 22);
+            this.txtFN.TabIndex = 2;
+            this.txtFN.TextChanged += new System.EventHandler(this.txtFN_TextChanged);
+            // 
+            // txtLN
+            // 
+            this.txtLN.Location = new System.Drawing.Point(6, 144);
+            this.txtLN.Name = "txtLN";
+            this.txtLN.Size = new System.Drawing.Size(240, 22);
+            this.txtLN.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(6, 221);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 34);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(506, 323);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 34);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // img
+            // 
+            this.img.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("img.ImageStream")));
+            this.img.TransparentColor = System.Drawing.Color.Transparent;
+            this.img.Images.SetKeyName(0, "file.jpg");
+            // 
+            // btnDel
+            // 
+            this.btnDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDel.Location = new System.Drawing.Point(152, 221);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(94, 34);
+            this.btnDel.TabIndex = 5;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 369);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tvShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvShow;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFN;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtLN;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ImageList img;
+        private System.Windows.Forms.Button btnDel;
+    }
+}
+
---
+++ Bai8/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Bai8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Are you sure to exit?",
+                "Confirm",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+                Close();
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtFN.Text) && !string.IsNullOrEmpty(txtLN.Text))
+            {
+                TreeNode selectedNode = tvShow.SelectedNode;
+                if (selectedNode != null)
+                {
+                    TreeNode childNode = new TreeNode(txtFN.Text.Trim() + ", " + txtLN.Text.Trim());
+                    selectedNode.Nodes.Add(childNode);
+                    txtFN.Text = "";
+                    txtLN.Text = "";
+                    txtFN.Focus();
+                }
+                else
+                    MessageBox.Show("Bạn chưa chọn file để thêm");
+            }
+            else
+                MessageBox.Show("Chưa nhập đủ thông tin");
+            foreach (TreeNode node in tvShow.Nodes)
+            {
+                node.BackColor = SystemColors.Window;
+                node.ForeColor = SystemColors.WindowText;
+            }
+            tvShow.SelectedNode = null;
+        }
+
+        private void txtFN_TextChanged(object sender, EventArgs e)
+        {
+            string name = txtFN.Text.Trim();
+            if (!string.IsNullOrEmpty(name) && char.IsLetter(name[0]))
+            {
+                char firstChar = char.ToUpper(name[0]);
+                foreach (TreeNode node in tvShow.Nodes) 
+                {
+                    char nodeFitstChar = Convert.ToChar(node.Text);
+                    if (nodeFitstChar == firstChar)
+                    {
+                        node.BackColor = SystemColors.Highlight;
+                        node.ForeColor = SystemColors.HighlightText;
+                        tvShow.SelectedNode = node;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+            {
+                tvShow.Nodes.Add(letter.ToString());
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            tvShow.SelectedNode.Remove();
+        }
+    }
+}
